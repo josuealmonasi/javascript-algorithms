b8917fdcd9b25710ed2b4dce0b8bed3f3b444e2d
Josué Almonasi <josue.almonasi@hotmail.com>
2023-04-23 19:35:19 -0600

    added comments

--- 01-Frequency-counters/01-Frequency-counter.js
+++ 01-Frequency-counters/01-Frequency-counter.js
@@ -1,12 +1,14 @@
 /*  Frequency Counter Pattern - sameFrequency */
 /*  Given two arrays, let's check if one contains all the squared values of the first one */
 /* in such ways that same([1, 2, 3, 2], [9, 1, 4, 4]) returns true */
+/* same([1, 2, 3, 2], [9, 1, 4, 9]) returns false */
 /* and same([1, 2, 3, 2], [9, 1, 4, 6]) returns false */
 function same(arr1, arr2) {
   // if the length of the arrays are not the same, return false
   if (arr1.length !== arr2.length) {
     return false
   }
+  // loop through the first array
   for (let i = 0; i < arr1.length; i++) {
     let correctIndex = arr2.indexOf(arr1[i] ** 2) // looks for the index of the squared value
     if (correctIndex === -1) {
