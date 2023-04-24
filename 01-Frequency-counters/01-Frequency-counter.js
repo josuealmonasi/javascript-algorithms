33496f0c3cdde1929e906d1106e9477948c08b78
Josué Almonasi <josue.almonasi@hotmail.com>
2023-04-23 19:20:37 -0600

    initial commit

---
+++ 01-Frequency-counters/01-Frequency-counter.js
@@ -0,0 +1,21 @@
+/*  Frequency Counter Pattern - sameFrequency */
+/*  Given two arrays, let's check if one contains all the squared values of the first one */
+/* in such ways that same([1, 2, 3, 2], [9, 1, 4, 4]) returns true */
+/* and same([1, 2, 3, 2], [9, 1, 4, 6]) returns false */
+function same(arr1, arr2) {
+  // if the length of the arrays are not the same, return false
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2) // looks for the index of the squared value
+    if (correctIndex === -1) {
+      // if the value is not found, return false
+      return false
+    }
+    arr2.splice(correctIndex, 1) // if the value is found, remove it from the array
+  }
+  return true // if the loop finishes, return true
+}
+
+module.exports = same
---
+++ 01-Frequency-counters/index.js
@@ -0,0 +1,4 @@
+const same = require('./01-Frequency-counter.js')
+
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4])) // true
+console.log(same([1, 2, 3, 2], [9, 1, 4, 5])) // false
